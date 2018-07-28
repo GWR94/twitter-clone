169ad4ff7b6b2275d2106e797c3f63df2305e8b7
James <james_gower@outlook.com>
2018-07-28 12:33:05 +0100

    changed dist location in indexjs

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ require('./routes/twitterRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets like our main.js file, or main.css
     // file!
-    app.use(express.static('client/build'));
+    app.use(express.static('client/public/dist'));
 
     // Express will serve up the index.html file if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     });
 }
 
