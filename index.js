e9631fb75f6a91bf597c99d49cb74dc13488d71b
James <james_gower@outlook.com>
2018-07-28 13:57:30 +0100

    updated package.json

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ require('./routes/twitterRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets like our main.js file, or main.css
     // file!
-    app.use(express.static(__dirname + '/client/public/dist'));
+    app.use(express.static(__dirname + 'client/public/'));
 
     // Express will serve up the index.html file if it doesn't recognize the route
     const path = require('path');
