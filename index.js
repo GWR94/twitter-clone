9f50e3251328b26b0e92ee5457858d0a8f0945d9
James <james_gower@outlook.com>
2018-07-28 13:00:08 +0100

    fixed dev script

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ require('./routes/twitterRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets like our main.js file, or main.css
     // file!
-    app.use(express.static(`${__dirname}/client/public/dist`));
+    app.use(express.static(__dirname + '/client/public/dist'));
 
     // Express will serve up the index.html file if it doesn't recognize the route
     const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'dist', 'index.html'));
     });
 }
 
