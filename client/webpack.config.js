5da3f1dfdb45c8296893622e1e6b0f893e860464
James <james_gower@outlook.com>
2018-07-30 09:22:45 +0100

    removed dist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
       "babel-polyfill", "./src/app.js"
     ],
     output: {
-      path: path.join(__dirname, "public", "dist"),
+      path: path.join(__dirname, "public"),
       filename: "bundle.js"
     },
     module: {
