d6388d8ac21e355a5baffc326a3da03ddcda853b
James <james_gower@outlook.com>
2018-07-30 09:14:15 +0100

    changed script tags in index.html

--- index.js
+++ index.js
@@ -31,13 +31,9 @@ require('./routes/databaseRoutes')(app);
 require('./routes/twitterRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets like our main.js file, or main.css
-    // file!
-    app.use(express.static(path.join(__dirname, 'client', 'public', 'dist')));
-
     // Express will serve up the index.html file if it doesn't recognize the route
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client/public/index.html'));
     });
 }
 
